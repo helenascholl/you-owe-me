1c620d3652f322429910ec5f3be4971b52ca8e24
schollsebastian <scholl.sebastian@gmx.at>
2021-04-26 19:14:38 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DebtorDetailComponent } from './debtor-detail/debtor-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'debtors/:id', component: DebtorDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
