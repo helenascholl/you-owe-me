3b9d240cb60531b7bdc97fd9b5e4624de6acbec7
schollsebastian <scholl.sebastian@gmx.at>
2021-04-26 19:28:31 +0200

    Move functionality of AppComponent to DebtorListComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { DebtorService } from './debtor.service';
-import Debtor from './debtor';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  public debtors: Debtor[];
-
-  constructor(
-    public debtorService: DebtorService
-  ) {
-    this.debtors = [];
-  }
-
-   ngOnInit(): void {
-     this.debtorService.getDebtors()
-       .subscribe(debtors => this.debtors = debtors);
-   }
+  constructor() { }
 
 }
