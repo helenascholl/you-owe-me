4c31e28bde89c18512a6282c3d5cd680935093ca
schollsebastian <scholl.sebastian@gmx.at>
2021-04-26 20:51:33 +0200

    Add angular materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DebtorDetailComponent } from './debtor-detail/debtor-detail.component';
 import { DebtorListComponent } from './debtor-list/debtor-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { DebtorListComponent } from './debtor-list/debtor-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
