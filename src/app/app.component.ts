a3a5892c1001c7ec67d2357be54b44e86bf71177
schollsebastian <scholl.sebastian@gmx.at>
2021-04-26 13:33:06 +0200

    List debtors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Debtor from './debtor';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'you-owe-me';
+
+  public debtors: Debtor[] = [
+    new Debtor('test1'),
+    new Debtor('test2')
+  ];
+
 }
