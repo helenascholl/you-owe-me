aa17aeead6f2ba61d401b8032f484299b5d1bae0
schollsebastian <scholl.sebastian@gmx.at>
2021-04-26 19:52:55 +0200

    Delete debtor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DebtorComponent } from './debtor/debtor.component';
 import { DebtorDetailComponent } from './debtor-detail/debtor-detail.component';
 import { DebtorListComponent } from './debtor-list/debtor-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DebtorComponent,
     DebtorDetailComponent,
     DebtorListComponent
   ],
