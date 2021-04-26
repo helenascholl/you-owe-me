18e4cab530554b2bd324706c7a89851c4dbc70bb
schollsebastian <scholl.sebastian@gmx.at>
2021-04-26 13:32:14 +0200

    Create class Debt

--- src/app/debt.ts
+++ src/app/debt.ts
@@ -4,3 +4,15 @@ export enum DebtType {
   MONTHLY,
   YEARLY
 }
+
+export class Debt {
+
+  public amount: number;
+  public type: DebtType;
+
+  constructor(amount: number, type: DebtType) {
+    this.amount = amount;
+    this.type = type;
+  }
+
+}
