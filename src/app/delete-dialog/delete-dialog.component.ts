369cfac6b4e2c9a739405bc44b6fea499915274b
schollsebastian <scholl.sebastian@gmx.at>
2021-04-30 17:33:58 +0200

    Create component delete-dialog

---
+++ src/app/delete-dialog/delete-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete-dialog',
+  templateUrl: './delete-dialog.component.html',
+  styleUrls: ['./delete-dialog.component.scss']
+})
+export class DeleteDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<DeleteDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { name: string }
+  ) { }
+
+  close(shouldBeDeleted: boolean): void {
+    this.dialogRef.close(shouldBeDeleted);
+  }
+
+}
