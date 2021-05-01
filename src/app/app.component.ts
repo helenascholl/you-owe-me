2e8877807f897d3bbfc9538240edb72106e7c787
schollsebastian <scholl.sebastian@gmx.at>
2021-05-01 17:36:04 +0200

    Add automatic redirect if not logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public fireAuth: AngularFireAuth,
+    public router: Router,
+    public zone: NgZone
+  ) { }
+
+  ngOnInit(): void {
+    this.fireAuth.onAuthStateChanged(user => {
+      if (!user) {
+        this.zone.run(() => this.router.navigate(['/login']));
+      }
+    });
+  }
 
 }
