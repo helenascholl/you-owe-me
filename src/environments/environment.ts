ea9b16006e16d77b1a4f90a0af40d80cd62d8c2f
schollsebastian <scholl.sebastian@gmx.at>
2021-05-01 17:24:55 +0200

    Add firebaseConfig to env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: false,
   locale: 'en-GB',
-  currencySymbol: '€'
+  currencySymbol: '€',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDDVSLZdHCV_yLhzE8mwpTjzBkFSvhf2A0',
+    authDomain: 'you-owe-me-4f611.firebaseapp.com',
+    projectId: 'you-owe-me-4f611',
+    storageBucket: 'you-owe-me-4f611.appspot.com',
+    messagingSenderId: '286409233199',
+    appId: '1:286409233199:web:79a09c588e5108b9b307da'
+  }
 };
