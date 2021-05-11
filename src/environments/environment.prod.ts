911451b62554cae4d40f7f7728dbbb41749b5e42
schollsebastian <scholl.sebastian@gmx.at>
2021-05-11 19:51:55 +0200

    Add databaseUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,9 +5,10 @@ export const environment = {
   firebaseConfig: {
     apiKey: 'AIzaSyDDVSLZdHCV_yLhzE8mwpTjzBkFSvhf2A0',
     authDomain: 'you-owe-me-4f611.firebaseapp.com',
+    databaseURL: 'https://you-owe-me-4f611-default-rtdb.europe-west1.firebasedatabase.app',
     projectId: 'you-owe-me-4f611',
     storageBucket: 'you-owe-me-4f611.appspot.com',
     messagingSenderId: '286409233199',
-    appId: '1:286409233199:web:79a09c588e5108b9b307da'
+    appId: '1:286409233199:web:1bd942ae503c9aa5b307da'
   }
 };
